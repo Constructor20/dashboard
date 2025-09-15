be9490618cd477c782fea13fb26ac36615c597af
Constructor20 <christophealeixo69@gmail.com>
2025-09-15 23:31:27 +0200

    Change link on Dashboard / add change password on profile

--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -4,8 +4,8 @@ import DashboardCircle from "./components/DashboardCircle.jsx";
 const sections = [
   { name: "Serveur", color: "#4f46e5", links: [{ text: "NAS", href: "#" }, { text: "Minecraft", href: "https://php.chrisdashboard.ddnsfree.com" }] },
   { name: "Home Assistant", color: "#16a34a", links: [{ text: "Etat maison", href: "#" }, { text: "WOL", href: "#" }] },
-  { name: "Réseau", color: "#eab308", links: [{ text: "Portainer", href: "#" }, { text: "cAdvisor", href: "#" }] },
-  { name: "Pub", color: "#ef4444", links: [{ text: "Portfolio", href: "#" }, { text: "LinkedIn", href: "#" }] },
+  { name: "Réseau", color: "#eab308", links: [{ text: "Portainer", href: "http://100.90.244.79:9000" }, { text: "cAdvisor", href: "#" }] },
+  { name: "Pub", color: "#ef4444", links: [{ text: "Portfolio", href: "#" }, { text: "LinkedIn", href: "https://www.linkedin.com/in/christophe-aleixo-14b5a4256" }] },
   { name: "Coming", color: "#0ea5e9", links: [{ text: "Soon", href: "#" }] },
 ];
 
